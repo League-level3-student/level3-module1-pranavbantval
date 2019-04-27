6888b654f93d0a2a6f9bc3f80358b79bed47ec29
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-04-26 18:32:18 -0700

    guestbook (IP)

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -4,20 +4,44 @@ import java.util.ArrayList;
 
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> names = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		names.add("Alex");
+		names.add("Bob");
+		names.add("Cans");
+		names.add("Delta");
+		names.add("E");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println(names.get(i));
+		}
 
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 4. Print all the Strings using a for-each loop
+		for (String namess : names) {
+			System.out.println(namess);
+		}
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < names.size(); i++) {
+			if (i % 2 == 1) {
+				System.out.println(names.get(i));
+			} else {
+
+			}
+		}
+		// 6. Print all the Strings in reverse order.
+		for (int i = names.size(); i > 0; i--) {
+			System.out.println(names.get(i - 1));
+		}
+
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < names.size(); i++) {
+			for (int j = 0; j < names.get(i).length(); j++) {
+				if (names.get(i).charAt(j) == 'e' || names.get(i).charAt(j) == 'E') {
+					System.out.println(names.get(i));
+				}
+			}
+		}
 	}
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class _02_GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,7 +18,55 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame f = new JFrame();
+	JButton add = new JButton();
+	JButton view = new JButton();
+ArrayList <String> guests = new ArrayList <String>();
+String ng;
+	public static void main(String[] args) {
+		
+		_02_GuestBook gb = new _02_GuestBook();
+		gb.guests.add("Guest #1: Bob Banders");
+		gb.guests.add("Guest #2: Sandy Summers");
+		gb.guests.add("Guest #3: Greg Ganders");
+		gb.guests.add("Guest #4: Donny Doners");
+		gb.UI();
+	}
 	
+	public void UI() {
+		
+		f.add(add);
+		f.add(view);
+		add.setText("Add Name");
+		view.setText("View Names");
+		add.setSize(500,250);
+		view.setSize(500,250);
+		f.setSize(500, 500);
+		f.show(true);
+		view.setLocation(0, 0);
+		add.setLocation(0, 250);
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+		System.out.println("a");
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton bp = (JButton) e.getSource();
+		
+		if(add==bp) {
+		ng= 	JOptionPane.showInputDialog("Add a name to the Guest Book with the following format: Guest#X: name");
+		
+		guests.add(ng);
+		System.out.println("b");
+		}
+		if(view==bp) {
+			JOptionPane.showMessageDialog(null, guests);
+			System.out.println("1");
+		}
+	}
 	
 	
 }

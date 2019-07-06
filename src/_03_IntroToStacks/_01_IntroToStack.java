d5551c004320b7b87de9eb530d74d8c8c0857edb
League Student <league@iMac-18.attlocal.net>
2019-07-05 18:30:38 -0700

    intro to stack, text undo redo(IP)

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -7,46 +7,58 @@ import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
 	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
+		// 1. Create a Stack of Doubles
+		// Don't forget to import the Stack class
 		Stack<Double> numbers = new Stack<Double>();
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		Random r = new Random();
-		for (int i = 0; i < 100; i++) {
-		numbers.push(r.nextDouble()*100);
+		for (int i = 0; i <= 100; i++) {
+			numbers.push(r.nextDouble() * 100);
 		}
-		
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive.
 		String num1 = JOptionPane.showInputDialog("Enter a number between 0 and 100 inclusive");
 		String num2 = JOptionPane.showInputDialog("Enter a number between 0 and 100 inclusive");
 		Double n1 = Double.parseDouble(num1);
 		Double n2 = Double.parseDouble(num2);
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
-		int x = numbers.size();
-		System.out.println("Popping elements off stack... \n Elements between" + num1 +" and "+ num2);
-		for (int i = x; i > 0; i--) {
-			if(numbers.get(x)>n1&&numbers.get(i)<n2) {
-				System.out.println(numbers.get(i));
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that
+		// is
+		// between the two numbers entered by the user, print it to the screen.
+		// int x = numbers.size();
+
+		System.out.println("Popping elements off stack... \n Elements between " + num1 + " and " + num2);
+		if (n1 < n2) {
+			for (int i = 0; i < 100; i++) {
+				Double x = numbers.pop();
+				if (x > n1 && x < n2) {
+					
+					System.out.println(x);
+				}
+
 			}
-			else if(numbers.get(x)<n1&&numbers.get(i)>n2) {
-				System.out.println(numbers.get(i));
+
+		} else {
+			for (int i = 0; i < 100; i++) {
+				Double x = numbers.pop();
+				if (x < n1 && x > n2) {
+					
+					System.out.println(x);
+				}
+
 			}
-			numbers.pop();
 		}
-		
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
+
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
 	}
 }
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,17 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
+	 *
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
 	 * 
 	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
@@ -14,5 +22,39 @@ public class _02_TextUndoRedo {
 	 * 
 	 * */
 	
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JLabel l = new JLabel();
+	public static void main(String[] args) {
+		_02_TextUndoRedo x =new _02_TextUndoRedo();
+		x.createUI();
 	
+	}
+	public void createUI() {
+		f.add(p);
+		p.add(l);
+		l.setText("");
+		f.show(true);
+		f.pack();
+		
+	}
+	
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int k = e.getKeyChar();
+		System.out.println(k);
+		
+	}
 }
